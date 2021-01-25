662983d56376c83da35e2ba2b4b67ec8ee0bfb68
Darlingnotin <darlingnotin@hotmail.com>
2021-01-25 01:28:58 -0500

    Uploading data

---
+++ tivoliCloudMonacoEditor.js
@@ -0,0 +1,81 @@
+// Copyright (c) 2021 Danielle Arlington.
+
+// This program is free software: you can redistribute it and/or modify  
+// it under the terms of the GNU General Public License as published by  
+// the Free Software Foundation, version 3.
+// https://www.gnu.org/licenses/gpl-3.0.en.html
+
+var tablet = Tablet.getTablet("com.highfidelity.interface.tablet.system");
+var button = tablet.addButton({
+    text: "Editor"
+});
+var monacoEditorUrl = Script.resolvePath("node_modules/monaco-editor.html");
+
+var overlayWebWindow = new OverlayWebWindow({
+    title: "Editor",
+    source: "about:blank",
+    width: 1280,
+    height: 720,
+    visible: false
+});
+
+function onClicked() {
+    setActive(!button.getProperties().isActive);
+}
+
+button.clicked.connect(onClicked);
+
+overlayWebWindow.webEventReceived.connect(function (event) {
+    var webEventData = JSON.parse(event);
+    if (webEventData.action == "save") {
+        Assets.putAsset(
+            {
+                data: webEventData.fileData,
+                path: webEventData.fileName
+            },
+            function (error, result) {
+                if (error) {
+                    //print("ERROR: Data not uploaded or mapping not set");
+                } else {
+                    //print("URL: " + result.url);
+                }
+            }
+        );
+    } else if (webEventData.action == "loadScript") {
+        Assets.getAsset(
+            {
+                url: webEventData.fileName,
+                responseType: "text"
+            },
+            function (error, result) {
+                if (error) {
+                    var messageData = {
+                        action: "loadScriptResponse",
+                        fileData: "ERROR: Data not downloaded"
+                    };
+                    overlayWebWindow.emitScriptEvent(JSON.stringify(messageData));
+                } else {
+                    var messageData = {
+                        action: "loadScriptResponse",
+                        fileData: result.response
+                    };
+                    overlayWebWindow.emitScriptEvent(JSON.stringify(messageData));
+                }
+            }
+        );
+    }
+});
+
+function setActive(active) {
+    button.editProperties({ isActive: active });
+    overlayWebWindow.setURL(monacoEditorUrl);
+    overlayWebWindow.setVisible(active);
+}
+
+overlayWebWindow.closed.connect(function () {
+    setActive(false);
+});
+
+Script.scriptEnding.connect(function () {
+    tablet.removeButton(button);
+});
